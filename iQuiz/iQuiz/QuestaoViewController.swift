f7a7db0ed3ca59ad9eef113dc35705627dba7c18
João Marcelo Candido Borges <joaomarcelocandido42@gmail.com>
2023-09-18 20:32:44 -0300

    add QuestaoViewControler

---
+++ iQuiz/iQuiz/QuestaoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QuestaoViewController.swift
+//  iQuiz
+//
+//  Created by João Marcelo Candido Borges on 18/09/23.
+//
+
+import UIKit
+
+class QuestaoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
